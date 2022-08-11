0255250b50c35a6835fb52e5c2f9ed08867a1b42
Marco Rava <marco.rava94@gmail.com>
2022-08-11 20:48:34 +0200

    changed app title and icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-import logo from "../../logo.svg";
+import logo from "../../logo.png";
 import searchIcon from "../../assets/icons/search-icon.svg";
 import { SearchBar } from "../SearchBar/SearchBar";
 import "./Header.scss";
